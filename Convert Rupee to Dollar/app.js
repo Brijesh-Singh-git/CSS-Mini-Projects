7ce8978c0c8414820ca2f8357fb04c089730b6e1
Brijesh Singh <106757278+Brijesh-Singh-git@users.noreply.github.com>
2023-04-05 11:02:06 +0530

    Initial commit

---
+++ Convert Rupee to Dollar/app.js
@@ -0,0 +1,8 @@
+function convertToRs(ourForm){
+    ourForm.Rs.value = ourForm.dlr.value * 80
+    }
+
+    function convertToDollar(ourForm){
+    ourForm.dlr.value = ourForm.Rs.value / 80
+
+    }
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSS-Mini-Projects
+ A simple mini projects in CSS for every beginners
